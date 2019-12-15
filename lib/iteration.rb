eaf4c220dd13af697411b6eb2b15068945955fea
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-15 19:37:33 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -32,13 +32,4 @@ end
   row += 1 
 end 
   total 
-
-
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
 end
